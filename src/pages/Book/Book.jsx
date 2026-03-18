d2a06e5e0db4b07c8e960bba9711758a645dcc8e
Sayed Web <112sayedalsahab@gmail.com>
2026-03-18 22:05:16 +0600

    modify product card & adding react icons

--- src/pages/Book/Book.jsx
+++ src/pages/Book/Book.jsx
@@ -1,29 +1,31 @@
 import React, { use } from 'react'
+import { FaRegStarHalfStroke } from "react-icons/fa6";
 
-const Book = ({singleBook}) => {
+const Book = ({ singleBook }) => {
     // const bookdata = use(bookPromise)
- 
-const {bookName, author, image}=singleBook;
+
+    const { bookName, author, image, rating, category } = singleBook;
 
     return (
-       <div className="card bg-base-100 w-96 shadow-sm">
-  <figure className='p-3'>
-    <img 
-      src={image}
-      alt="Shoes" />
-  </figure>
-  <div className="card-body">
-    <h2 className="card-title">
-      {bookName}
-      <div className="badge badge-secondary">NEW</div>
-    </h2>
-    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
-    <div className="card-actions justify-end">
-      <div className="badge badge-outline">Fashion</div>
-      <div className="badge badge-outline">Products</div>
-    </div>
-  </div>
-</div>
+        <div className="card bg-base-100 w-96 shadow-sm border p-6">
+            <figure className='p-4 bg-gray-100 w-2/3 mx-auto'>
+                <img
+                    className='h-[166px]'
+                    src={image}
+                    alt="Shoes" />
+            </figure>
+            <div className="card-body">
+                <h2 className="card-title">
+                    {bookName}
+                    <div className="badge badge-secondary">NEW</div>
+                </h2>
+                <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+                <div className="card-actions justify-end">
+                    <div className="badge badge-outline">{category}</div>
+                    <div className="badge badge-outline">{rating}<FaRegStarHalfStroke /></div>
+                </div>
+            </div>
+        </div>
     );
 };
 
