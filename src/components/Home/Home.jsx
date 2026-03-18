1074c6bbc5f10dbc66e3b12cdd4a54a85ed505e6
Sayed Web <112sayedalsahab@gmail.com>
2026-03-18 21:13:25 +0600

    load all books and display in the ui

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,16 @@
 import React, { use } from 'react'
 import Banner from '../Banner/Banner';
 import Books from '../../pages/Books/Books';
+import { useLoaderData } from 'react-router';
 
 const Home = () => {
-   
+   const data = useLoaderData();
+   console.log(data)
     return (
         <div>
         
             <Banner></Banner>
-            <Books></Books>
+            <Books data={data}></Books>
         </div>
     ); 
 }
